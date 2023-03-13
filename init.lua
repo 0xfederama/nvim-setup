645ab35b233f9d50de2381c40d08d50eb504b6b6
Federico <vgfede@gmail.com>
2023-03-13 14:41:04 +0100

    add todo-comments, add keymap descriptions

--- init.lua
+++ init.lua
@@ -41,9 +41,16 @@ require('packer').startup(function(use)
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+      }
+    end
+  }
+
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        signs = false,
       }
     end
   }
@@ -313,10 +320,10 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.setup_nvim_cmp({
-    preselect = 'none',
-    completion = {
-        completeopt = 'menu,menuone,noinsert,noselect'
-    },
+  preselect = 'none',
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect'
+  },
 })
 
 lsp.ensure_installed({
@@ -337,16 +344,26 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = '[F]ormat current 
 lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false, desc = "test" }
 
-  opts.desc = "[G]o to [D]efinition"; vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  opts.desc = "Show codelens"; vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  opts.desc = "Show symbol in wors[S]pace"; vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  opts.desc = "Open [D]iagnostics"; vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  opts.desc = "Next diagnostic"; vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  opts.desc = "Prev diagnostic"; vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  opts.desc = "[C]ode [A]ctions"; vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_actions() end, opts)
-  opts.desc = "[V]ariable [R]eferences"; vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  opts.desc = "[V]ariable [R]e[N]ame"; vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  opts.desc = "Get signature [HELP]"; vim.keymap.set("n", "<leader>help", function() vim.lsp.buf.signature_help() end, opts)
+  opts.desc = "[G]o to [D]efinition";
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  opts.desc = "Show codelens";
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  opts.desc = "Show symbol in wors[S]pace";
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  opts.desc = "Open [D]iagnostics";
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  opts.desc = "Next diagnostic";
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  opts.desc = "Prev diagnostic";
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  opts.desc = "[C]ode [A]ctions";
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_actions() end, opts)
+  opts.desc = "[V]ariable [R]eferences";
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  opts.desc = "[V]ariable [R]e[N]ame";
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  opts.desc = "Get signature [HELP]";
+  vim.keymap.set("n", "<leader>help", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
